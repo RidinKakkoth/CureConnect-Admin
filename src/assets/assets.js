5201439f53cd5813f7639a67ba24beb6eb6a4e0b
Ridin Kakkoth <k.ridin@ymail.com>
2024-10-14 16:48:25 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+export const assets = {
+    add_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781671/CureConnect/admin/add_icon_fwqkeh.svg",
+    admin_logo:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1728901952/CureConnect/admin/cureconnect-high-resolution-logo-transparent_fy2htz.png",
+    appointment_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781666/CureConnect/admin/appointment_icon_nzhpjt.svg",
+    cancel_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781667/CureConnect/admin/cancel_icon_dhwllq.svg",
+    doctor_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781667/CureConnect/admin/doctor_icon_in38bf.svg",
+    upload_area:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781671/CureConnect/admin/upload_area_zhqtbm.svg",
+    home_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781668/CureConnect/admin/home_icon_jxumst.svg",
+    patients_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781669/CureConnect/admin/patients_icon_vyhjnp.svg",
+    people_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781670/CureConnect/admin/people_icon_z6kti7.svg",
+    list_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781668/CureConnect/admin/list_icon_hvevwc.svg",
+    tick_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781670/CureConnect/admin/tick_icon_kvdvzx.svg",
+    appointments_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781666/CureConnect/admin/appointments_icon_ujmelr.svg",
+    earning_icon:"https://res.cloudinary.com/ddymh3cnk/image/upload/v1727781668/CureConnect/admin/earning_icon_isowdg.svg"
+}
